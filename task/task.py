4a65166c6a7f9afa73226d356fe6ce2a56ab5e8f
barnabassprague <barnabassprague@users.noreply.github.com>
2017-08-14 19:40:17 -0600

    Update task.py

--- task/task.py
+++ task/task.py
@@ -321,7 +321,7 @@ def main(img1_path, img2_path, out_dir,
     # write the status
     if input_data is not None:
         status = {'status': 'success', 'reason': 'task completed'}
-        with open('inputData.json, 'w') as outfile:
+        with open('inputData.json', 'w') as outfile:
             json.dump(input_data, outfile)
         with open('/mnt/work/status.json', 'w') as outfile:
             json.dump(status, outfile)
